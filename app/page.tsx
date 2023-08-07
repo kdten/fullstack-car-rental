4873eb33bf6aa0b89315c0ce19c2f7d08034d784
kdten <kolten.rasmussen@gmail.com>
2023-08-06 21:46:35 -0600

    nav, header footer, hero section done

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { Hero } from '@/components'
+
+export default function Home() {
+  return (
+    <main className="over-flow-hidden">
+
+      <Hero />
+
+    </main>
+  )
+}
