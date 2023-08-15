f88a4659b8188e46f3a272dec9e9fe8892de8388
kdten <kolten.rasmussen@gmail.com>
2023-08-15 11:01:50 -0600

    centered car details middle image

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -66,7 +66,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => (
                       <Image src={generateCarImageUrl(car, "29")} 
                       alt='car model' fill priority className='object-contain' />
                     </div>
-                    <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                    <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg mt-3'>
                       <Image src={generateCarImageUrl(car, "33")} 
                       alt='car model' fill priority className='object-contain' />
                     </div>
